71826336a3f87b29de190538c779ce7c92fa7ad2
Norberto Ortigoza <norberto@bunsan.io>
2019-02-21 16:11:29 -0600

    Este es el inicio de un gran proyecto.

---
+++ lib/nqcc.ex
@@ -0,0 +1,18 @@
+defmodule Nqcc do
+  @moduledoc """
+  Documentation for Nqcc.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Nqcc.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nqcc_test.exs
@@ -0,0 +1,8 @@
+defmodule NqccTest do
+  use ExUnit.Case
+  doctest Nqcc
+
+  test "greets the world" do
+    assert Nqcc.hello() == :world
+  end
+end
