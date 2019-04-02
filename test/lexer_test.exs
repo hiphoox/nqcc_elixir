a5e5da16596ac0df55e581c54761a16bc7f68693
Norberto Ortigoza <norberto@bunsan.io>
2019-04-01 20:30:20 -0600

    Adding 3 tests to the Lexer

---
+++ test/lexer_test.exs
@@ -0,0 +1,32 @@
+defmodule LexerTest do
+  use ExUnit.Case
+  doctest Lexer
+
+  setup_all do
+    {:ok,
+     tokens: [
+       :int_keyword,
+       :main_keyword,
+       :open_paren,
+       :close_paren,
+       :open_brace,
+       :return_keyword,
+       {:constant, 2},
+       :semicolon,
+       :close_brace
+     ]}
+  end
+
+  test "elements separated just by spaces", state do
+    assert Lexer.scan_words(["int", "main(){return", "2;}"]) == state[:tokens]
+  end
+
+  test "function name separated of function body", state do
+    assert Lexer.scan_words(["int", "main()", "{return", "2;}"]) == state[:tokens]
+  end
+
+  test "everything is separated", state do
+    assert Lexer.scan_words(["int", "main", "(", ")", "{", "return", "2", ";", "}"]) ==
+             state[:tokens]
+  end
+end
--- test/nqcc_test.exs
+++ test/nqcc_test.exs
@@ -2,7 +2,7 @@ defmodule NqccTest do
   use ExUnit.Case
   doctest Nqcc
 
-  test "greets the world" do
-    assert Nqcc.hello() == :world
-  end
+  # test "greets the world" do
+  #   assert Nqcc.hello() == :world
+  # end
 end
